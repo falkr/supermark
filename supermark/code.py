428e49f787e19e2b496d3b90e67111a47b1498de
falkr <kraemer@ntnu.no>
2023-07-14 15:35:13 +0200

    Support for code clipboard.

--- supermark/code.py
+++ supermark/code.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-from typing import Any, Dict, List
+from typing import Any, Dict, List, Sequence
 
 from pygments import highlight
 from pygments.formatters import LatexFormatter
@@ -21,6 +21,29 @@ class Code(Chunk):
     def get_chunk_type(self) -> str:
         return "code" if self.lang is None else f"code/{self.lang}"
 
+    def wrap_in_code_frame(self, code: str, builder: Builder) -> str:
+        html: List[str] = []
+        raw_id = "id" + Chunk.create_hash(self.code)
+        html.append('<div class="source-code-frame">')
+        html.append(f'<pre class="hidden-pre" id="{raw_id}">{self.code}</pre>')
+        html.append(
+            f'<button type="button" class="copy-button" data-clipboard-target="#{raw_id}">'
+        )
+        html.append(
+            '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-clipboard" viewBox="0 0 16 16">'
+        )
+        html.append(
+            '  		<path d="M4 1.5H3a2 2 0 0 0-2 2V14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3.5a2 2 0 0 0-2-2h-1v1h1a1 1 0 0 1 1 1V14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V3.5a1 1 0 0 1 1-1h1v-1z"></path>'
+        )
+        html.append(
+            '  		<path d="M9.5 1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5h3zm-3-1A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3z"></path>'
+        )
+        html.append("	</svg>")
+        html.append("</button>")
+        html.append(code)
+        html.append("</div>")
+        return "\n".join(html)
+
     def to_html(self, builder: Builder, target_file_path: Path) -> str:
         # lexer = None
         # if self.lang is not None:
@@ -33,7 +56,9 @@ class Code(Chunk):
         #     formatter = HtmlFormatter()
         #     output.append(highlight(self.code, lexer, formatter))
         #     return "\n".join(output)
-        return builder.convert_code(self.get_content(), target_format="html")
+        return self.wrap_in_code_frame(
+            builder.convert_code(self.get_content(), target_format="html"), builder
+        )
 
     def to_latex(self, builder: Builder) -> str:
         lexer = None
